efd23f316c7a7405cedace89c38a94ba5c04e0fe
shangwenhe <shangwenhe@users.noreply.github.com>
2017-03-13 18:12:28 +0800

    添加注释

--- to-Upper-Lower.js
+++ to-Upper-Lower.js
@@ -1,7 +1,8 @@
 'ABababc'.replace( /\w/g, function(a){ 
+  // 把a转成大写，如果 a === a 说明它本身是大写的
   if(a.toUpperCase() === a){ 
-    return a.toLowerCase() 
+    return a.toLowerCase();
   }else{
-    return a.toUpperCase() 
+    return a.toUpperCase();
   }
  });
